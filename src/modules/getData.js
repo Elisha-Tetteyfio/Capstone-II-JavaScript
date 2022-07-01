8b356ed89161b4aafaccf23edd933ba9a8cbce85
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-07-01 20:17:04 +0000

    Add number of likes to items

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,8 +1,18 @@
 const url = 'https://api.artic.edu/api/v1/artworks?page=10&limit=100';
+const likesURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/6MkPjjBjFG5Jhh37x3c8/likes'
 
-export default async function getData() {
+export async function getData() {
   try {
     const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function getLikes() {
+  try {
+    const response = await fetch(likesURL);
     return await response.json();
   } catch (error) {
     return false;
