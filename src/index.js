33ec3658179a4b9f7d1a239a7074da650bc71702
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-07-01 05:10:23 -0700

    Make changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import _ from 'lodash';
+import './style.css';
+import './main.scss';
+
 const addup = () => {
-  let a = 2;
-  let b = 2;
+  const a = 2;
+  const b = 2;
 
   return a + b;
-}
+};
 
 module.exports = addup;
