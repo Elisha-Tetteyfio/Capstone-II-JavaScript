8189eb9c9fd3c61fc3922c5d7bbb12e4e9c205af
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-07-02 18:22:26 -0700

    Make canges

--- src/index.js
+++ src/index.js
@@ -12,49 +12,12 @@ import {
   getBtn,
 } from './modules/addLike.js';
 
-
 const homeLink = document.querySelector('#homeLink');
 const logo = `<img src="${Logo}" alt="o" id="logo"></img>`;
 homeLink.innerHTML += logo;
 
-
-const items = document.querySelector('#items');
-const getArt = async () => {
-  const result = await getData();
-
-  result.data.forEach((element) => {
-    const listItem = document.createElement('li');
-
-    const itemImage = document.createElement('img');
-    itemImage.setAttribute('src', `https://www.artic.edu/iiif/2/${element.image_id}/full/200,/0/default.jpg`);
-    itemImage.setAttribute('alt', 'image of artwork');
-
-    const productTitle = document.createElement('p');
-    productTitle.classList.add('fw-bold');
-    productTitle.innerHTML = `${element.title}`;
-
-    listItem.appendChild(itemImage);
-    listItem.appendChild(productTitle);
-
-    const likesWrapper = document.createElement('div');
-
-    const likeIcon = document.createElement('i');
-
-    const likesText = document.createElement('p');
-    likesText.innerHTML = '5 Likes';
-
-    likesWrapper.appendChild(likeIcon);
-    likesWrapper.appendChild(likesText);
-
-    const commentButton = document.createElement('button');
-    commentButton.classList.add('comment-btn');
-    commentButton.setAttribute('type', 'button');
-    commentButton.innerHTML = 'Comment';
-
-    listItem.appendChild(likesWrapper);
-    listItem.appendChild(commentButton);
-    items.appendChild(listItem);
-  });
+const Buildpage = async () => {
+  getArt();
 
   const activateCommentBtn = async () => {
     const result = await getData();
@@ -78,5 +41,5 @@ const getArt = async () => {
 };
 
 getArt();
+Buildpage();
 getBtn();
-
